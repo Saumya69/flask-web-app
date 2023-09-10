32561c8207617abf10bfca9edbc9338b7bcd9b13
Saumya <saumyamary69@gmail.com>
2023-09-10 22:09:29 +0530

    db creation & user login 4th commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+#define db
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def  create_app():
     app = Flask(__name__)
     #encrypt your session and cookies data
     app.config['SECRET_KEY'] = 'sanasaumya'
+    #stores db in website folder
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
     
     from .views import views
     from .auth import auth
@@ -11,4 +21,25 @@ def  create_app():
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
     
+    from .models import User, Note
+    with app.app_context():
+        db.create_all()
+        
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    
+    
     return app
+
+#check if db exists or not
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 #standard routes of the website////blueprint allows to define routes in multiple files 
 from flask import Flask, render_template
 from flask import Blueprint
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
